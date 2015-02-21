35f8aef9794978485e97aa9490bbfb39aa7cc26b
Robert Marwil <robertmarwil@Roberts-MacBook-Air.local>
2015-02-20 18:51:29 -0700

    configure register login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/register' => 'registrations#new'
+  post '/register' => 'registrations#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
