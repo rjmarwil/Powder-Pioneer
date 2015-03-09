fbbc0133a4464cc5884000808222ffd5c4b3a5fd
Jake Marwil <rjm02006@gmail.com>
2015-03-09 17:35:13 -0600

    define strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,7 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
+    @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
@@ -14,8 +14,10 @@ class RegistrationsController < ApplicationController
     end
   end
 
-  def register
-    @user = User.new
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
