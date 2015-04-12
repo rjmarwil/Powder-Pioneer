d53f791442c72bb39fc849a593a3d2abeed8f355
Jake Marwil <rjm02006@gmail.com>
2015-04-11 18:57:21 -0600

    include access to location and info variables

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,13 @@
 class WelcomeController < ApplicationController
 
   def index
+    weather = Weather.new
+    if params[:location] == nil
+      @location = "Denver, CO"
+    else
+      @location = params[:location]
+    end
+    # @info = weather.five_day
   end
 
 end
