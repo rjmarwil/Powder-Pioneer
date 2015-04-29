83eb5971981bad51be499c4d0571b7d1b6f3d431
Jake Marwil <rjm02006@gmail.com>
2015-04-29 01:03:24 -0600

    add javascript functions for parallax scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,32 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap.min
+//= require init
+//= require jquery-1.6.1.min
 //= require bootstrap-sprockets
-//= require _main
 //= require_tree .
+
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+
+   $('section[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+
+        // background position
+        var coords = '50% '+ yPos + 'px';
+
+        // move the background
+        $scroll.css({ backgroundPosition: coords });
+      }); // end window scroll
+   });  // end section function
+}); // close out script
+
+document.createElement("section");
