b19ec7dd6e107bc42010084bb756b0eeaa93e717
Jake Marwil <rjm02006@gmail.com>
2015-06-07 14:28:58 -0600

    add smooth scrolling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,25 +18,39 @@
 //= require snow
 
 $(document).ready(function(){
-   // cache the window object
-   $window = $(window);
+  // Smooth scrolling for anchors
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 
-   $('section[data-type="background"]').each(function(){
-     // declare the variable to affect the defined data-type
-     var $scroll = $(this);
+  // cache the window object
+  $window = $(window);
 
-      $(window).scroll(function() {
-        // HTML5 proves useful for helping with creating JS functions!
-        // also, negative value because we're scrolling upwards
-        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+  $('section[data-type="background"]').each(function(){
+   // declare the variable to affect the defined data-type
+   var $scroll = $(this);
 
-        // background position
-        var coords = '50% '+ yPos + 'px';
+    $(window).scroll(function() {
+      // HTML5 proves useful for helping with creating JS functions!
+      // also, negative value because we're scrolling upwards
+      var yPos = -($window.scrollTop() / $scroll.data('speed'));
 
-        // move the background
-        $scroll.css({ backgroundPosition: coords });
-      }); // end window scroll
-   });  // end section function
+      // background position
+      var coords = '50% '+ yPos + 'px';
+
+      // move the background
+      $scroll.css({ backgroundPosition: coords });
+    }); // end window scroll
+  });  // end section function
 }); // close out script
 
 document.createElement("section");
