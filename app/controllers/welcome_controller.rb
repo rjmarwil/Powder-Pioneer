e878d4be3ff7d502a041989850ad290bfcd40bd7
Jake Marwil <rjm02006@gmail.com>
2015-06-07 18:03:57 -0600

    add new create and destroy methods and strong params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,39 @@
 class WelcomeController < ApplicationController
 
   def index
-    weather = Weather.new
-    if params[:location] == nil
-      @location = "Denver, CO"
-    else
-      @location = params[:location]
-    end
+    # weather = Weather.new
+    # if params[:location] == nil
+    #   @location = "Denver, CO"
+    # else
+    #   @location = params[:location]
+    # end
     # @info = weather.five_day
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      render json: @location.to_json
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @location = Location.find_by(params[:id])
+    @location.destroy
+    render nothing: true
+  end
+
+private
+
+  def location_params
+    params.require(:location).permit(:name, :difficulty, :riskiness, :description)
   end
 
 end
